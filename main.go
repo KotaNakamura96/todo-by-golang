d346593005f42f1952e795f14128827395c59f97
kota1130 <kota.nkmr1130@gmail.com>
2024-09-12 23:31:55 +0900

    int型の基礎

--- main.go
+++ main.go
@@ -4,50 +4,14 @@ import (
 	"fmt"
 )
 
-var i5 int = 500
-
-func outer () {
-	var s4 string = "outer"
-	fmt.Println(s4)
-}
-
 func main() {
 	var i int = 100
-	fmt.Println(i)
 
-	var s string = "Hello Go"
-	fmt.Println(s)
+	var i2 int64 = 200
+	fmt.Println(i + 50)
+	fmt.Println(int(i2) + i)
 
-	var t, f bool = true, false
-	fmt.Println(t, f)
+	fmt.Printf("%T\n", i2)
 
-	var (
-		i2 int = 200
-		s2 string = "Golang"
-	)
-	fmt.Println(i2, s2)
-
-	var i3 int
-	var s3 string
-	fmt.Println(i3, s3)
-
-	i3 = 300
-	s3 = "Go"
-	fmt.Println(i3, s3)
-
-	i = 150
-	fmt.Println(i)
-
-	i4 := 400
-	fmt.Println(i4)
-
-	i4 = 450
-	fmt.Println(i4)
-
-	fmt.Println(i5)
-
-	outer()
-
-	var s5 string = "Not Use"
-	fmt.Println(s5)
+	fmt.Printf("%T\n", int32(i2))
 }
