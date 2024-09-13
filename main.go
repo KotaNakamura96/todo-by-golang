aa3578cba082f93058230ec9ffaf80424881a91a
kota1130 <kota.nkmr1130@gmail.com>
2024-09-13 23:21:12 +0900

    interface型の基礎

--- main.go
+++ main.go
@@ -5,34 +5,20 @@ import (
 )
 
 func main() {
-	var arr1 [3]int
-	fmt.Println(arr1)
-	fmt.Printf("%T\n", arr1)
+	var x interface{}
+	fmt.Println(x)
+	x = 1
+	fmt.Println(x)
 
-	var arr2 [3]string = [3]string{"A", "B"}
-	fmt.Println(arr2)
-	arr2 = [3]string{"AA", "BB"}
-	fmt.Println(arr2)
+	x = 3.14
+	fmt.Println(x)
 
-	arr3 := [3]int{1, 2, 3}
-	fmt.Println(arr3)
-	arr3 = [3]int{4, 5, 6}
-	fmt.Println(arr3)
+	x = "hogehoge"
+	fmt.Println(x)
 
-	arr4 := [...]string{"D"}
-	fmt.Println(arr4)
-	fmt.Printf("%T\n", arr4)
+	x = [3]int{1, 2, 3}
+	fmt.Println(x)
 
-	fmt.Println(arr2[0])
-	fmt.Println(arr2[1])
-	fmt.Println(arr2[2-1])
-
-	arr2[2] = "C"
-	fmt.Println(arr2)
-
-	// var arr5 [4]int
-	// arr5 = arr1
-	// fmt.Println(arr5)
-
-	fmt.Println(len(arr4))
+	x = 2
+	fmt.Println(x + 3)
 }
