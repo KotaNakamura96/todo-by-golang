12b099d66f3cb725b73ff443baaa6fdbe98fe043
kota1130 <kota.nkmr1130@gmail.com>
2024-09-13 23:30:41 +0900

    型変換の基礎

--- main.go
+++ main.go
@@ -2,23 +2,45 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
 func main() {
-	var x interface{}
-	fmt.Println(x)
-	x = 1
-	fmt.Println(x)
+	// var i int = 1
+	// fl64 := float64(i)
 
-	x = 3.14
-	fmt.Println(x)
+	// fmt.Println(fl64)
+	// fmt.Printf("i = %T\n", i)
+	// fmt.Printf("fl64 = %T\n", fl64)
 
-	x = "hogehoge"
-	fmt.Println(x)
+	// i2 := int(fl64)
+	// fmt.Println(i2)
+	// fmt.Printf("i2 = %T\n", i2)
 
-	x = [3]int{1, 2, 3}
-	fmt.Println(x)
+	// fl := 10.5
+	// i3 := int(fl)
+	// fmt.Println(i3)
+	// fmt.Printf("i3 = %T\n", i3)
 
-	x = 2
-	fmt.Println(x + 3)
+	var s string = "100"
+	fmt.Printf("s = %T\n", s)
+
+	i, _ := strconv.Atoi("b")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	fmt.Println(i)
+	fmt.Printf("i = %T\n", i)
+
+	var i2 int = 200
+	s2 := strconv.Itoa(i2)
+	fmt.Println(s2)
+	fmt.Printf("s2 = %T\n", s2)
+
+	var h string = "Hello Golang"
+	b := []byte(h)
+	fmt.Println(b)
+
+	h2 := string(b)
+	fmt.Println(h2)
 }
