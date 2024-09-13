812f9fc539d91e75240a6b622ca8d7e287f4eb26
kota1130 <kota.nkmr1130@gmail.com>
2024-09-13 23:05:54 +0900

    string型の基礎

--- main.go
+++ main.go
@@ -5,6 +5,21 @@ import (
 )
 
 func main() {
-	var t, f bool = true, false
-	fmt.Println(t, f)
+	var s string = "Hello Golang"
+	fmt.Println(s)
+	fmt.Printf("%T\n", s)
+
+	var si string = "300"
+	fmt.Println(si)
+	fmt.Printf("%T\n", si)
+
+	fmt.Println(`test
+	test
+		test
+	`)
+
+	fmt.Println("\"a\"")
+	fmt.Println(`"`)
+
+	fmt.Println(string(s[0]))
 }
