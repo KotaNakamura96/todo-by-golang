f60d682f614e666c5d3112b8f275e23e13982acb
kota1130 <kota.nkmr1130@gmail.com>
2024-09-14 23:29:45 +0900

    論理演算の基礎

--- main.go
+++ main.go
@@ -3,8 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(1 == 1)
-	fmt.Println(1 == 2)
-	fmt.Println(4 <= 8)
-	fmt.Println(true != false)
+	fmt.Println(true && false == true)
+	fmt.Println(true && true == true)
+	fmt.Println(true || true == false)
 }
