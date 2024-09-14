81a6129b99cd66875d76b4d2f436c4d5ea74d08d
kota1130 <kota.nkmr1130@gmail.com>
2024-09-14 23:28:14 +0900

    比較演算の基礎

--- main.go
+++ main.go
@@ -3,15 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(1 + 2)
-	fmt.Println("ABC" + "DE")
-
-	fmt.Println(5 - 1)
-	fmt.Println(2 * 4)
-	fmt.Println(60 / 6)	
-	fmt.Println(9 % 4)
-
-	n := 0
-	n += 4
-	fmt.Println(n)
+	fmt.Println(1 == 1)
+	fmt.Println(1 == 2)
+	fmt.Println(4 <= 8)
+	fmt.Println(true != false)
 }
