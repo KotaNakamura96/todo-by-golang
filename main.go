d9e6a489f012e783741f5fbf3c701178e0dfac82
kota1130 <kota.nkmr1130@gmail.com>
2024-09-14 23:26:28 +0900

    算術演算の基礎

--- main.go
+++ main.go
@@ -2,38 +2,16 @@ package main
 
 import "fmt"
 
-const Pi = 3.14
-const (
-	URL = "http://xxx.co.jp"
-	SiteName = "test"
-)
-const (
-	A = 1
-	B
-	C
-	D = "Dです"
-	E
-	F
-)
-
-// var Big int = 9223372036854775807 + 1
-const Big = 9223372036854775807 + 1
-
-const (
-	c0 = iota
-	c1
-	c2
-)
-
 func main() {
-	fmt.Println(Pi)
+	fmt.Println(1 + 2)
+	fmt.Println("ABC" + "DE")
 
-	// Pi = 3
-	// fmt.Println(Pi)
+	fmt.Println(5 - 1)
+	fmt.Println(2 * 4)
+	fmt.Println(60 / 6)	
+	fmt.Println(9 % 4)
 
-	fmt.Println(URL, SiteName)
-	fmt.Println(A, B, C, D, E, F)
-	fmt.Println(Big - 1)
-
-	fmt.Println(c0, c1, c2)
+	n := 0
+	n += 4
+	fmt.Println(n)
 }
