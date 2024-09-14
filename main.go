7483cc37ded94c6ef3d25cb375158d90053000c9
kota1130 <kota.nkmr1130@gmail.com>
2024-09-14 23:23:40 +0900

    定数の基礎

--- main.go
+++ main.go
@@ -1,46 +1,39 @@
 package main
 
-import (
-	"fmt"
-	"strconv"
+import "fmt"
+
+const Pi = 3.14
+const (
+	URL = "http://xxx.co.jp"
+	SiteName = "test"
+)
+const (
+	A = 1
+	B
+	C
+	D = "Dです"
+	E
+	F
+)
+
+// var Big int = 9223372036854775807 + 1
+const Big = 9223372036854775807 + 1
+
+const (
+	c0 = iota
+	c1
+	c2
 )
 
 func main() {
-	// var i int = 1
-	// fl64 := float64(i)
+	fmt.Println(Pi)
 
-	// fmt.Println(fl64)
-	// fmt.Printf("i = %T\n", i)
-	// fmt.Printf("fl64 = %T\n", fl64)
+	// Pi = 3
+	// fmt.Println(Pi)
 
-	// i2 := int(fl64)
-	// fmt.Println(i2)
-	// fmt.Printf("i2 = %T\n", i2)
+	fmt.Println(URL, SiteName)
+	fmt.Println(A, B, C, D, E, F)
+	fmt.Println(Big - 1)
 
-	// fl := 10.5
-	// i3 := int(fl)
-	// fmt.Println(i3)
-	// fmt.Printf("i3 = %T\n", i3)
-
-	var s string = "100"
-	fmt.Printf("s = %T\n", s)
-
-	i, _ := strconv.Atoi("b")
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-	fmt.Println(i)
-	fmt.Printf("i = %T\n", i)
-
-	var i2 int = 200
-	s2 := strconv.Itoa(i2)
-	fmt.Println(s2)
-	fmt.Printf("s2 = %T\n", s2)
-
-	var h string = "Hello Golang"
-	b := []byte(h)
-	fmt.Println(b)
-
-	h2 := string(b)
-	fmt.Println(h2)
+	fmt.Println(c0, c1, c2)
 }
