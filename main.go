099d597c160913d469df738ec18a78d479b2371c
kota1130 <kota.nkmr1130@gmail.com>
2024-09-15 13:02:36 +0900

    無名関数の基礎

--- main.go
+++ main.go
@@ -2,35 +2,17 @@ package main
 
 import "fmt"
 
-func Plus(x, y int) int {
-	return x + y
-}
-
-func Div(x, y int) (int, int) {
-	q := x / y
-	r := x % y
-	return q, r
-}
-
-// 変数名も指定することができる.その場合はどの変数をreturnするかは省略できる.
-func Double(price int) (result int) {
-	result = price * 2
-	return
-}
-
-func Noreturn() {
-	fmt.Println("No Return.")
-}
-
 func main() {
-	i1 := Plus(1, 4)
-	fmt.Println(i1)
+	f := func(x, y int) int {
+		return x + y
+	}
 
-	i2, _ := Div(9, 4)
+	i := f(10, 1)
+	fmt.Println(i)
+
+	i2 := func(x, y int ) int {
+		return x + y
+	}(1, 2)
+
 	fmt.Println(i2)
-
-	i4 := Double(10)
-	fmt.Println(i4)
-
-	Noreturn()
 }
