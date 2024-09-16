1215ea5d364afe76c96c23da3817accfc06f0a8f
kota1130 <kota.nkmr1130@gmail.com>
2024-09-16 12:44:29 +0900

    switch文の基礎

--- main.go
+++ main.go
@@ -1,53 +1,14 @@
 package main
 
-import (
-	"fmt"
-)
-
-
 
 func main() {
-	// i := 0
-	// for {
-	// 	i++
-	// 	if i == 3 {
-	// 		break
-	// 	}
-	// 	fmt.Println("loop")
+	// n := 3
+	// switch n {
+	// case 1, 2:
+	// 	fmt.Println("1 or 2")
+	// case 3, 4:
+	// 	fmt.Println("3 or 4")
+	// default:
+	// 	fmt.Println("default")
 	// }
-
-	// point := 0
-	// for point < 10 {
-	// 	fmt.Println(point)
-	// 	point++
-	// }
-
-	// for i := 0; i < 10; i++ {
-	// 	if i % 3 == 0 {
-	// 		continue
-	// 	}
-	// 	fmt.Println(i)
-	// }
-
-	// arr := [3]int{1,2,3}
-	// for i:=0; i < len(arr); i++ {
-	// 	fmt.Println(arr[i])
-	// }
-
-	// arr := [3]int{1,2,3}
-
-	// for i := range arr {
-	// 	fmt.Println(i)
-	// }
-
-	// sl := []string{"Python", "PHP", "Go"}
-	// for i, v := range sl {
-	// 	fmt.Println(i, v)
-	// }
-
-	m := map[string]int{"apple": 100, "banana": 200}
-	
-	for k, v := range m {
-		fmt.Println(k, v)
-	}
 }
